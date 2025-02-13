7e37164a7fe135c2a528f749f26693b845179fd9
GeebergyStatic <staticbusiness1411@gmail.com>
2025-02-13 08:04:31 +0100

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -942,7 +942,7 @@ router.post('/send-email', async (req, res) => {
 // Endpoint to fetch all usernames
 router.get('/usernames', async (req, res) => {
     try {
-        const usernames = await User.find({}, 'username'); // Fetch only the 'username' field
+        const usernames = await User.find({}, { username: 1, _id: 0 }); // Include 'username', exclude '_id'
         res.json(usernames);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
